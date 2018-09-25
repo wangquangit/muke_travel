dfe1216bc753c4f2ef8cfb959fb8cd3983f85d0d
wangquanit <304587090@qq.com>
2018-09-25 12:03:31 +0800

    create search components
    创建了城市选择组件,但有1像素问题有待解决

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+        <input type="text" placeholder="输入城市名或拼音" class="search-input">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .search
+        padding: 0 .1rem
+        height : .72rem
+        background : $bgColor
+        .search-input
+            box-sizing : border-box
+            height : .62rem
+            line-height : .62rem
+            width : 100%
+            text-align : center
+            border-radius : .15rem
+            color : #666
+            padding: 0 .2rem
+</style>
