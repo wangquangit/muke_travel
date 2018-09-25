fb11574b454ed481e5e1953279090630c26a3ea4
wangquanit <304587090@qq.com>
2018-09-25 11:45:37 +0800

    create city component
    创建了城市选择组件,创建了header组件

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arro-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arro-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -41,6 +43,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .arro-icon
         font-size : .24rem
         margin-left : -.04rem
