e873d7468727ace7c495fb832850b4462c912771
wangquanit <304587090@qq.com>
2018-09-25 19:54:46 +0800

    change city select
    城市选择页使用keepalive优化网页性能

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
@@ -25,21 +26,21 @@ export default {
     HomeRecommend,
     HomeWeekend
   },
-  mounted () {
-    // 生命周期钩子函数
-    this.getHomeInfo()
-  },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+      axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
       res = res.data
@@ -49,6 +50,17 @@ export default {
         this.recommendList = res.data.recommendList
         this.weekendList = res.data.weekendList
       }
+    }
+  },
+  mounted () {
+    // 生命周期钩子函数
+    this.lastCity = this.city
+    this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
     }
   }
 }
