33a8e72a8e8c3a11abe193714647caa29dcc3c00
wangquanit <304587090@qq.com>
2018-09-26 15:46:27 +0800

    change detail components
    详情页-动态获取详情页数据,但存在一个BUG,详情页下拉后无法渐现头部信息

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner :sightName="sightName" :bannerImg="bannerImg" :gallaryImgs="gallaryImgs"></detail-banner>
     <detail-header></detail-header>
     <div class="content">
-      <detail-list :list="list"></detail-list>
+      <detail-list :categoryList="categoryList"></detail-list>
     </div>
   </div>
 </template>
@@ -12,8 +12,10 @@
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
 import DetailList from './components/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
+  // 组件名name的作用:1.递归组件时,2.取消缓存时,3.定义组件时
   components: {
     DetailBanner,
     DetailHeader,
@@ -21,12 +23,32 @@ export default {
   },
   data () {
     return {
-      list: [
-        {title: '成人票', children: [{title: '成人五馆联票', children: [{title: '成人五馆联票 - 某一连锁店销售'}]}, {title: '成人七馆联票'}]},
-        {title: '学生票'},
-        {title: '儿童票'},
-        {title: '特惠票'}
-      ]
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      categoryList: []
+
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
+  },
+  methods: {
+    // 发送ajax请求
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {id: this.$route.params.id}
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.categoryList = data.categoryList
+      }
     }
   }
 }
